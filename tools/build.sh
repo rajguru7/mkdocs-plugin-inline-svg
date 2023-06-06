c6fa86f6675e38bd1e2b7f12ec5ef0140f351fb4
Saurabh Rajguru <saurabhrajguru@Saurabhs-MacBook-Air.local>
2023-06-06 17:58:20 +0530

    Restructured the repo

---
+++ tools/build.sh
@@ -0,0 +1,4 @@
+# /bin/sh
+python3 tools/setup.py bdist_wheel -d build
+#pip3 uninstall build/mkdocs_plugin_inline_svg_mod-0.1.0-py3-none-any.whl
+#pip3 install build/mkdocs_plugin_inline_svg_mod-0.1.0-py3-none-any.whl
--- tools/setup.py
+++ tools/setup.py
@@ -1,7 +1,17 @@
 # setup.py
 
 import os
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Command
+
+class CleanCommand(Command):
+    """Custom clean command to tidy up the project root."""
+    user_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        os.system('rm -vrf ./*.egg-info')
 
 def read_file(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
@@ -10,7 +20,7 @@ setup(
     name='mkdocs-plugin-inline-svg-mod',
     version='0.1.0',
     description='A MkDocs plugin that inlines SVG images into the output.',
-	long_description=read_file('README.md'),
+	long_description=read_file('../README.md'),
     long_description_content_type='text/markdown',
     keywords='mkdocs plugin inline svg',
     url='https://github.com/rajguru7/mkdocs-plugin-inline-svg-mod',
@@ -26,5 +36,8 @@ setup(
         'mkdocs.plugins': [
             'inline-svg = src:InlineSvgPlugin',
         ]
+    },
+    cmdclass={
+        'clean': CleanCommand,
     }
-)
+    )
